1e1c9d273b333f333a447005d85157eac028a107
AFKAR <afkarmohamme356@gmail.com>
2023-03-05 15:56:05 +0530

    (16th commit)

--- payment2.java
+++ payment2.java
@@ -1,7 +1,8 @@
 public class payment2 {
     public static void main(String args[]){
 
-        System.out.println("Hello Payment!!!!!!!!");
+        System.out.println("Hello Payment!!!!!!!!!");
+        System.out.println("Hello Payment2");
 
     }
 
